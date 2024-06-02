0e2287002d0cfdadc85c6397ca98c31d7b62152b
Francesco Bonizzi <f.bonizzi@outlook.it>
2024-06-02 22:54:03 +0200

    Spritesheet

--- Web/src/assets/AssetsLoader.ts
+++ Web/src/assets/AssetsLoader.ts
@@ -1,30 +1,72 @@
-import {AnimatedSprite, Assets, Sprite} from "pixi.js";
+import {AnimatedSprite, Assets, Sprite, Spritesheet} from "pixi.js";
 import InfartAssets from "./InfartAssets";
+
+const _assetsRoot: string = '/assets';
+const _imagesAssetsRoot: string = `${_assetsRoot}/images`;
+const _spriteSheetsAssetsRoot: string = `${_imagesAssetsRoot}/spriteSheets`;
+
+const loadSpriteFromFile = async (name: string): Promise<Sprite> => {
+    const texture = await Assets.load(`${_imagesAssetsRoot}/${name}`);
+    return new Sprite(texture);
+}
+
+const loadSpriteFromSpriteSheet = (spriteSheet: Spritesheet, name: string): Sprite => {
+    const texture = spriteSheet.textures[`${name}.png`];
+    return new Sprite(texture);
+}
+
+const loadMenuSprite = async (name: string): Promise<Sprite> => {
+    return await loadSpriteFromFile(`menu/${name}.png`);
+}
+
+const loadSpriteSheet = async (name: string): Promise<Spritesheet> => {
+    const spriteSheet = await Assets.load(`${_spriteSheetsAssetsRoot}/${name}.json`);
+    return spriteSheet as Spritesheet;
+}
 
 export const loadAssets = async (): Promise<InfartAssets> => {
 
-    // Menu
-    const texture = await Assets.load(
-        "/assets/images/menuBackground.png"
-    );
-    const menuBackground = new Sprite(texture);
+    // Single image sprites
+    const menuBackground = await loadMenuSprite("menuBackground");
+    const gameTitle = await loadMenuSprite("gameTitle");
+    const scoreBackground = await loadMenuSprite("scoreBackground");
+    const gameOverBackground = await loadMenuSprite("gameOverBackground");
 
-    // SpriteSheet
-    const playerSpriteSheet = await Assets.load('/assets/images/spriteSheets/playerTexturesSpriteSheet.json');
-
-    const anim = new AnimatedSprite(playerSpriteSheet.animations.playerRun);
-    anim.animationSpeed = 0.3;
-    anim.play();
-
-    const singleFrame = new Sprite(playerSpriteSheet.textures['jalapenos']);
+    // Sprite sheets
+    const playerSpriteSheet = await loadSpriteSheet("player");
 
     return {
         menu: {
-            background: menuBackground
+            background: menuBackground,
+            gameTitle: gameTitle,
+            scoreBackground: scoreBackground,
+            gameOverBackground: gameOverBackground
         },
-        jalapeno: singleFrame,
+        bang: loadSpriteFromSpriteSheet(playerSpriteSheet, "bang"),
+        broccoloParticle: loadSpriteFromSpriteSheet(playerSpriteSheet, "broccoloParticle"),
+        bean: loadSpriteFromSpriteSheet(playerSpriteSheet, "bean"),
+        burger: loadSpriteFromSpriteSheet(playerSpriteSheet, "burger"),
+        gameOver: loadSpriteFromSpriteSheet(playerSpriteSheet, "gameOver"),
+        jalapenoParticle: loadSpriteFromSpriteSheet(playerSpriteSheet, "jalapenoParticle"),
+        jalapenos: loadSpriteFromSpriteSheet(playerSpriteSheet, "jalapenos"),
+        merda: loadSpriteFromSpriteSheet(playerSpriteSheet, "merda"),
+        pause: loadSpriteFromSpriteSheet(playerSpriteSheet, "pause"),
+        play: loadSpriteFromSpriteSheet(playerSpriteSheet, "play"),
+        record: loadSpriteFromSpriteSheet(playerSpriteSheet, "record"),
+        scoreggiaParticle: loadSpriteFromSpriteSheet(playerSpriteSheet, "scoreggiaParticle"),
+        stella: loadSpriteFromSpriteSheet(playerSpriteSheet, "stella"),
+        verdura: loadSpriteFromSpriteSheet(playerSpriteSheet, "verdura"),
+        background: loadSpriteFromSpriteSheet(playerSpriteSheet, "background"),
+        deathScreen: loadSpriteFromSpriteSheet(playerSpriteSheet, "deathScreen"),
+        nuvola1: loadSpriteFromSpriteSheet(playerSpriteSheet, "nuvola1"),
+        nuvola2: loadSpriteFromSpriteSheet(playerSpriteSheet, "nuvola2"),
+        nuvola3: loadSpriteFromSpriteSheet(playerSpriteSheet, "nuvola3"),
         player: {
-            run: anim,
+            run: new AnimatedSprite(playerSpriteSheet.animations.playerRun),
+            idle: new AnimatedSprite(playerSpriteSheet.animations.playerIdle),
+            fart: new AnimatedSprite(playerSpriteSheet.animations.playerFart),
+            fall: new AnimatedSprite(playerSpriteSheet.animations.playerFall),
+            merda: new AnimatedSprite(playerSpriteSheet.animations.playerMerda),
         }
     }
 };
--- Web/src/assets/InfartAssets.ts
+++ Web/src/assets/InfartAssets.ts
@@ -3,11 +3,36 @@ import { AnimatedSprite, Sprite } from "pixi.js";
 interface InfartAssets {
     menu: {
         background: Sprite;
+        gameTitle: Sprite;
+        scoreBackground: Sprite;
+        gameOverBackground: Sprite;
     },
     player: {
         run: AnimatedSprite;
+        idle: AnimatedSprite;
+        fart: AnimatedSprite;
+        fall: AnimatedSprite;
+        merda: AnimatedSprite;
     },
-    jalapeno: Sprite;
+    bang: Sprite;
+    broccoloParticle: Sprite;
+    bean: Sprite;
+    burger: Sprite;
+    gameOver: Sprite;
+    jalapenoParticle: Sprite;
+    jalapenos: Sprite;
+    merda: Sprite,
+    pause: Sprite;
+    play: Sprite;
+    record: Sprite;
+    scoreggiaParticle: Sprite;
+    stella: Sprite;
+    verdura: Sprite;
+    background: Sprite;
+    deathScreen: Sprite;
+    nuvola1: Sprite;
+    nuvola2: Sprite;
+    nuvola3: Sprite;
 }
 
 export default InfartAssets;
