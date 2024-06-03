79f5b1d9bb94a30fe2183665167fb47c2821ab1e
Francesco Bonizzi <f.bonizzi@outlook.it>
2024-06-04 00:15:56 +0200

    Grattacieli strato 1

--- Web/src/background/GrattacieliGroup.ts
+++ Web/src/background/GrattacieliGroup.ts
@@ -4,20 +4,39 @@ import GrattacieliAutogeneranti from "./GrattacieliAutogeneranti.ts";
 
 class GrattacieliGroup {
 
+    private _grattacieliBack: GrattacieliAutogeneranti;
+    private _grattacieliMid: GrattacieliAutogeneranti;
     private _grattacieliGround: GrattacieliAutogeneranti;
+
 
     constructor(
         app: Application<Renderer>,
         infartAssets: InfartAssets) {
 
+        this._grattacieliBack = new GrattacieliAutogeneranti(
+            app,
+            infartAssets.sprites.buildings.back);
+
+        this._grattacieliMid = new GrattacieliAutogeneranti(
+            app,
+            infartAssets.sprites.buildings.mid);
+
         this._grattacieliGround = new GrattacieliAutogeneranti(
             app,
             infartAssets.sprites.buildings.ground);
+
     }
 
     update(time: Ticker) {
-        const dx = time.deltaTime * 0.5;
-        this._grattacieliGround.moveX(-dx);
+        const dx = -time.deltaTime;
+
+        this._grattacieliBack.moveX(dx * 0.1);
+        this._grattacieliBack.update();
+
+        this._grattacieliMid.moveX(dx * 0.3);
+        this._grattacieliMid.update();
+
+        this._grattacieliGround.moveX(dx * 0.5);
         this._grattacieliGround.update();
     }
 }
