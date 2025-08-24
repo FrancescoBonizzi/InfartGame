a82228b4aa0059ca50856d2fedcbbf20bf39bccf
Francesco Bonizzi <f.bonizzi@outlook.it>
2025-08-24 22:20:06 +0200

    Remove redundant comments in `main.ts` for cleaner code readability.

--- Web/src/main.ts
+++ Web/src/main.ts
@@ -4,10 +4,8 @@ import LoadingThing from "./uiKit/LoadingThing.ts";
 import Game from "./Game.ts";
 
 (async () => {
-    // Create a PixiJS application.
     const app = new Application();
 
-    // Intialize the application.
     await app.init({
         background: '#1CB3DE',
         width: 800,
@@ -17,7 +15,6 @@ import Game from "./Game.ts";
         autoDensity: true,
     });
 
-    // Then adding the application's canvas to the DOM body.
     document.body.appendChild(app.canvas);
 
     try {
