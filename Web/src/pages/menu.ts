12e263874387181edbdf143797c1b9e763dad3b9
Francesco Bonizzi <f.bonizzi@outlook.it>
2025-08-25 19:07:35 +0200

    Redesign menu layout: update CSS structure, refactor HTML, and rename assets for improved organization and visuals.

--- Web/src/pages/menu.ts
+++ Web/src/pages/menu.ts
@@ -2,13 +2,18 @@ import router from './router';
 
 export function renderMenuPage(container: HTMLElement) {
     container.innerHTML = `
-<main class="main">
-    <section class="card">
-        <h1 class="title">INFART</h1>
-        <div class="actions">
-            <a href="/game" class="btn primary" data-navigo>Gioca</a>
-            <a href="/scores" class="btn" data-navigo>Punteggi</a>
+<main>
+    <section class="menu">
+        
+        <div class="menu-content">
+            <img src="/public/assets/images/menu/infart-game-title.png" alt="Game menu title" />
+            
+            <nav class="actions">
+                <a href="/game" class="btn primary" data-navigo>Gioca</a>
+                <a href="/scores" class="btn" data-navigo>Punteggi</a>
+            </nav>
         </div>
+        
     </section>
 </main>
 `;
